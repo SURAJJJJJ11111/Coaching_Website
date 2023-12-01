1aeda78f28ea90d529ec7c5316bd3717c9516b2f
SURAJ <145916198+SURAJJJJJ11111@users.noreply.github.com>
2023-12-01 21:12:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// // JavaScript functionality
+// document.getElementById('startLearning').addEventListener('click', function () {
+//     alert('You have started your learning journey!');
+// });
+
+// // Smooth scrolling for navigation links
+// const navLinks = document.querySelectorAll('nav a');
+
+// navLinks.forEach((link) => {
+//     link.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         const targetId = link.getAttribute('href').substring(1);
+//         const targetSection = document.getElementById(targetId);
+//         window.scrollTo({
+//             top: targetSection.offsetTop - 80,
+//             behavior: 'smooth',
+//         });
+//     });
+// });
+
+// // Course enrollment handling
+// const enrollButtons = document.querySelectorAll('.enroll');
+
+// enrollButtons.forEach((button) => {
+//     button.addEventListener('click', function () {
+//         alert('You have successfully enrolled in this course!');
+//     });
+// });
